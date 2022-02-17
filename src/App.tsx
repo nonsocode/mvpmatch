15b7b1aff1da5aed28504932fd78cb1e88c71f70
Nonso Chukwuogor <nonso.chukwuogor@shortcut.com>
2022-02-17 04:50:45 -0800

    Basic Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import { useState } from "react";
-import {BrowserRouter} from 'react-router-dom'
+import styled from '@emotion/styled'
+import { useState } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import AppLogo from './components/layout/AppLogo'
+import Header from './components/layout/Header'
+import Navbar from './components/layout/Navbar'
+
+const Layout = styled.div({
+  display: 'grid',
+  width: '100%',
+  height: '100vh',
+  gridTemplate: `
+  'logo   header'    80px
+  'nav    content'   1fr 
+  'nav    footer'    60px / 90px 1fr`,
+})
+
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  
-  return <></>;
-};
+  const [count, setCount] = useState(0)
 
-export default App;
+  return (
+    <BrowserRouter>
+      <Layout>
+        <AppLogo/>
+        <Header />
+        <Navbar />
+      </Layout>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/icons/defaults.ts
@@ -0,0 +1,8 @@
+import { IconProps, IconSize } from 'src/types/icon'
+
+export const defaults: IconProps = {
+  primaryColor: "#005B97",
+  secondaryColor: "#2DAEE5",
+  tertiaryColor: "#ffffff",
+  size: IconSize.MEDIUM
+}
